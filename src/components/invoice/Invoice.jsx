e5301c184207444b2eaec485fe390c72ae204320
Abhishek Yadav <monuy8830@gmail.com>
2025-04-07 14:57:19 +0530

    123

--- src/components/invoice/Invoice.jsx
+++ src/components/invoice/Invoice.jsx
@@ -4,6 +4,7 @@ import { TableCell, TableBody, TableRow } from "@windmill/react-ui";
 const Invoice = ({ data, currency, getNumberTwo }) => {
   return (
     <>
+    
       <TableBody className="bg-white dark:bg-gray-800 divide-y divide-gray-100 text-serif text-sm ">
         {data?.cart?.map((item, i) => (
           <TableRow key={i} className="dark:border-gray-700 dark:text-gray-400">
